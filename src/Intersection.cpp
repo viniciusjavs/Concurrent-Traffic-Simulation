30cfa83a7e0be3433b81ccae88582329c48c2dae
Vinicius Sa <viniciusjavs@gmail.com>
2020-04-23 10:30:42 -0300

    Lesson 3 Exercise [worked]

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -10,9 +10,6 @@
 
 /* Implementation of class "WaitingVehicles" */
 
-// L3.1 : Safeguard all accesses to the private members _vehicles and _promises with an appropriate locking mechanism,
-// that will not cause a deadlock situation where access to the resources is accidentally blocked.
-
 int WaitingVehicles::getSize()
 {
     return _vehicles.size();
@@ -20,6 +17,7 @@ int WaitingVehicles::getSize()
 
 void WaitingVehicles::pushBack(std::shared_ptr<Vehicle> vehicle, std::promise<void> &&promise)
 {
+    const std::lock_guard<std::mutex> lock(_mutex);
     _vehicles.push_back(vehicle);
     _promises.push_back(std::move(promise));
 }
@@ -33,6 +31,7 @@ void WaitingVehicles::permitEntryToFirstInQueue()
     // fulfill promise and send signal back that permission to enter has been granted
     firstPromise->set_value();
 
+    const std::lock_guard<std::mutex> lock(_mutex);
     // remove front elements from both queues
     _vehicles.erase(firstVehicle);
     _promises.erase(firstPromise);
@@ -69,7 +68,8 @@ std::vector<std::shared_ptr<Street>> Intersection::queryStreets(std::shared_ptr<
 // adds a new vehicle to the queue and returns once the vehicle is allowed to enter
 void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
 {
-    // L3.3 : Ensure that the text output locks the console as a shared resource. Use the mutex _mtxCout you have added to the base class TrafficObject in the previous task. Make sure that in between the two calls to std-cout at the beginning and at the end of addVehicleToQueue the lock is not held.
+    // Ensure that the text output locks the console as a shared resource.
+    const std::lock_guard<std::mutex> lock(TrafficObject::_mtxCout);
 
     std::cout << "Intersection #" << _id << "::addVehicleToQueue: thread id = " << std::this_thread::get_id() << std::endl;
 
