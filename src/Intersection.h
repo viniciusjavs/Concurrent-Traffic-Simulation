24e9cb5d604c5cf613b20c3148d74ca612fd3cdb
Vinicius Sa <viniciusjavs@gmail.com>
2020-04-24 01:12:36 -0300

    Add a TrafficLight object to the Intersection class

--- src/Intersection.h
+++ src/Intersection.h
@@ -6,6 +6,7 @@
 #include <mutex>
 #include <memory>
 #include "TrafficObject.h"
+#include "TrafficLight.h"
 
 // forward declarations to avoid include cycle
 class Street;
@@ -41,7 +42,7 @@ public:
     void addVehicleToQueue(std::shared_ptr<Vehicle> vehicle);
     void addStreet(std::shared_ptr<Street> street);
     std::vector<std::shared_ptr<Street>> queryStreets(std::shared_ptr<Street> incoming); // return pointer to current list of all outgoing streets
-    void simulate();
+    void simulate() override;
     void vehicleHasLeft(std::shared_ptr<Vehicle> vehicle);
     bool trafficLightIsGreen();
 
@@ -54,6 +55,7 @@ private:
     std::vector<std::shared_ptr<Street>> _streets;   // list of all streets connected to this intersection
     WaitingVehicles _waitingVehicles; // list of all vehicles and their associated promises waiting to enter the intersection
     bool _isBlocked;                  // flag indicating wether the intersection is blocked by a vehicle
+    TrafficLight _trafficLight;
 };
 
 #endif
